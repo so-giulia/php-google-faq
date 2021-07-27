103adad72877b9459407785bbd05498a82aae362
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-07-27 19:27:27 +0200

    add: structure part 1 - header/footer

--- index.php
+++ index.php
@@ -4,10 +4,87 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
+
     <title>Domande Frequenti - Google Replica</title>
 </head>
+
 <body>
-    tessssst
+    <!-- header start -->
+    <header>
+        <!-- header upper start -->
+        <div class="header-upper head-padding flex-row justify-between align-center">
+
+            <!-- logo + title start -->
+            <div class="head-title-container flex-row align-center">
+                <img src="img/logo.png" alt="Google logo extended">
+                <h2>Privacy e termini</h2>
+            </div>
+            <!-- logo + title end -->
+            
+            <!-- user start -->
+            <div class="user-container flex-row align-center">
+       
+                <i class="fas fa-ellipsis-v"></i>
+                <i class="fas fa-ellipsis-v"></i>
+                <i class="fas fa-ellipsis-v"></i>
+               
+                <img src="img/user.jpg" alt="User icon">
+            </div>
+            <!-- user end -->
+        </div>
+        <!-- header upper end -->
+
+
+        <!-- header lower start -->
+        <div class="header-lower">
+            <!-- nav start -->
+            <nav class="main-nav">
+                <ul class="flex-row justify-between align-center head-padding ul-reset">
+                    <li><a href="#">Introduzione</a></li>
+                    <li><a href="#">Norme sulla privacy</a></li>
+                    <li><a href="##">Termini di servizio</a></li>
+                    <li><a href="#">Tecnologie</a></li>
+                    <li><a class="nav-active" href="#">Domande frequenti</a></li>
+                </ul>
+            </nav>
+            <!-- nav end -->
+        </div>
+        <!-- header lower end -->
+    </header>
+    <!-- header end -->
+
+    <!-- main start -->
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+    <!-- main end -->
+
+    <!-- footer start -->
+    <footer>
+        <div class="container">
+            <div class="flex-row justify-between">
+                <ul class="ul-reset flex-row">
+                    <li><a href="#">Google</a></li>
+                    <li>·</li>
+                    <li><a href="#">Tutto su Google</a></li>
+                    <li>·</li>
+                    <li><a href="#">Privacy</a></li>
+                    <li>·</li>
+                    <li><a href="#">Termini</a></li>
+                </ul>
+
+                <span>temp</span>
+            </div>  
+        </div>
+    </footer>
+    <!-- footer end -->
 </body>
 </html>
